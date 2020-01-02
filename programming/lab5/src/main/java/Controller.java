3ded57426778aa89e26ad413711a685a4d810669
yank0vy3rdna <bibiwa2001@gmail.com>
2020-01-02 22:48:37 +0300

    lab5 added javadoc

--- programming/lab5/src/main/java/Controller.java
+++ programming/lab5/src/main/java/Controller.java
@@ -1,3 +1,16 @@
+
+/**
+ * Controller
+ * <ul>
+ * <li>import {filename} - добавить в коллекцию все элементы из файла</li>
+ * <li>info - вывести информацию о коллекции</li>
+ * <li>insert {key} {json} - добавить новый элемент с заданным ключом</li>
+ * <li>show - вывести все элементы коллекции в строковом представлении в консоль</li>
+ * <li>remove {key} - удалить элемент коллекции по его ключу</li>
+ * <li>remove greater {json} - удалить элементы коллекции, превышающие заданный</li>
+ * <li>remove greater key {key} - удалить элементы коллекции, ключ которых превышает данный</li>
+ * </ul>
+ */
 class Controller {
 	private final Domain manager;
 	Controller(Domain manager){
@@ -9,6 +22,13 @@ class Controller {
 	public void end(String filename){
 		manager.saveXML(filename);
 	}
+
+	/**
+	 *
+	 * метод, выполняющий различные действия в зависимости от введенной команды
+	 * @param command
+	 * @return output
+	 */
 	public String execute(String command){
 		String[] operands = command.split(" ",2);
 		switch(operands[0]){
--- programming/lab5/src/main/java/Storage.java
+++ programming/lab5/src/main/java/Storage.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 public class Storage {
     private  Date createDate;
